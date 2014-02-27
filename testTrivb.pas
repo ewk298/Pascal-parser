eb657335142b8affdb652257991c1e4a69c6ef51
U-nico-PC\nico <nico@nico-PC.(none)>
2014-02-27 15:46:05 -0600

    working on block

---
+++ testTrivb.pas
@@ -0,0 +1,3 @@
+{my second lexer doesn't work with multiline comments. test with original trivb.pas before turnin}
+program graph1(output);
+begin i:=j; if i+j then x:=a+b*c else x:=a*b+c; k:=i end.
